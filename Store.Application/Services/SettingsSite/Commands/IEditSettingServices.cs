623b11e860919088d0b1933d2bd21e5a3a2b87a7
mosys2 <mohammadsaadati117@gmail.com>
2023-07-22 11:10:49 +0430

    SettingServices

---
+++ Store.Application/Services/SettingsSite/Commands/IEditSettingServices.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces.Contexs;
+using Store.Common.Constant;
+using Store.Common.Dto;
+using Store.Domain.Entities.Settings;
+using Store.Domain.Entities.Translate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Application.Services.SettingsSite.Commands
+{
+    public interface IEditSettingServices
+    {
+        Task<ResultDto> Execute(EditSettingDto request, string languageId);
+    }
+    public class EditSettingServices : IEditSettingServices
+    {
+        private readonly IDatabaseContext _context;
+        public EditSettingServices(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultDto> Execute(EditSettingDto request, string languageId)
+        {
+            var language = await _context.Languages.FindAsync(languageId);
+            if (language == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = MessageInUser.NotFind
+                };
+            }
+            var settingItem = await _context.Settings.FirstOrDefaultAsync();
+            if (settingItem == null)
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = MessageInUser.NotFind
+                };
+            }
+            settingItem.BaseUrl = request.BaseUrl;
+            settingItem.ShowPerPage = request.ShowPerPage;
+            settingItem.Logo2 = request.Logo2;
+            settingItem.MetaTags = request.MetaTags;
+            settingItem.Description= request.Description;
+            settingItem.Logo= request.Logo;
+            settingItem.Icon= request.Icon;
+            settingItem.LanguageId = languageId;
+            settingItem.SiteName = request.SiteName;
+            settingItem.UpdateTime=DateTime.Now;
+            await _context.SaveChangesAsync();
+
+            return new ResultDto
+            {
+                IsSuccess = true,
+                Message=MessageInUser.UploadSuccess
+            };
+
+        }
+    }
+    public class EditSettingDto
+    {
+        public string? SiteName { get; set; }
+        public string? BaseUrl { get; set; }
+        public string? Logo { get; set; }
+        public string? Logo2 { get; set; }
+        public string? Icon { get; set; }
+        public int ShowPerPage { get; set; }
+        public string? MetaTags { get; set; }
+        public string? Description { get; set; }
+        public string LanguageId { get; set; }
+    }
+}
---
+++ Store.Application/Services/SettingsSite/Queries/IGetSettingServices.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces.Contexs;
+using Store.Common.Constant;
+using Store.Common.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Application.Services.SettingsSite.Queries
+{
+    public interface IGetSettingServices
+    {
+        Task<ResultDto<SettingDto>> Execute(string languageId);
+    }
+
+    public class GetSettingServices : IGetSettingServices
+    {
+
+        private readonly IDatabaseContext _context;
+        public GetSettingServices(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultDto<SettingDto>> Execute(string languageId)
+        {
+            var language = await _context.Languages.FindAsync(languageId);
+            if (language == null)
+            {
+                return new ResultDto<SettingDto>
+                {
+                    IsSuccess = false,
+                    Message = MessageInUser.NotFind
+                };
+            }
+            var settingItem = await _context.Settings
+                .Where(p => p.LanguageId == language.Id)
+                .Select(p => new SettingDto
+                {
+                    BaseUrl = p.BaseUrl,
+                    Description = p.Description,
+                    LanguageId = languageId,
+                    Icon = p.Icon,
+                    Logo = p.Logo,
+                    Logo2 = p.Logo2,
+                    MetaTags = p.MetaTags,
+                    ShowPerPage = p.ShowPerPage,
+                    SiteName = p.SiteName
+                }).FirstOrDefaultAsync();
+
+            if (settingItem == null)
+            {
+                return new ResultDto<SettingDto>
+                {
+                    IsSuccess = false,
+                    Message = MessageInUser.NotFind
+                };
+            }
+
+            return new ResultDto<SettingDto>
+            {
+                Data = settingItem,
+                IsSuccess = true
+            };
+        }
+    }
+
+    public class SettingDto
+    {
+        public string? SiteName { get; set; }
+        public string? BaseUrl { get; set; }
+        public string? Logo { get; set; }
+        public string? Logo2 { get; set; }
+        public string? Icon { get; set; }
+        public int ShowPerPage { get; set; }
+        public string? MetaTags { get; set; }
+        public string? Description { get; set; }
+        public string? LanguageId { get; set; }
+    }
+}
