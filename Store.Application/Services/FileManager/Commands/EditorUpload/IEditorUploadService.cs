789f355ee8e560fe7c767070c70c58f7faea7366
mosys2 <mohammadsaadati117@gmail.com>
2023-08-03 15:06:24 +0430

    fixbbbb

--- Store.Application/Services/FileManager/Commands/EditorUpload/IEditorUploadService.cs
+++ Store.Application/Services/FileManager/Commands/EditorUpload/IEditorUploadService.cs
@@ -36,10 +36,11 @@ namespace Store.Application.Services.FileManager.Commands.EditorUpload
 					string username = _configuration.GetSection("FtpUsername").Value;
 					string password = _configuration.GetSection("FtpPassword").Value;
 					string ftpRoot = _configuration.GetSection("FtpEditor").Value;
-					ftpRoot="wwwroot/"+ftpRoot;
 					string BaseUrl = _configuration.GetSection("BaseUrl").Value;
-					string url = ftpRoot + directoryPath;
-					client.Host = ftpServer;
+					string url = "wwwroot/"+ftpRoot + directoryPath;
+					string returnUrl = ftpRoot + directoryPath;
+
+                    client.Host = ftpServer;
 					client.Credentials = new NetworkCredential(username, password);
 					List<string> upload = new List<string>();
 					foreach (var file in files)
@@ -49,7 +50,7 @@ namespace Store.Application.Services.FileManager.Commands.EditorUpload
 						{
 							client.UploadStream(stream, remoteFilePath);
 						}
-						upload.Add(BaseUrl+"/"+remoteFilePath);
+						upload.Add(BaseUrl+"/"+returnUrl+ "/" + file.FileName);
 
 					}
 					client.Disconnect();
