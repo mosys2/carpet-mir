b4d32df19844d2455d28250ee1e10d8b54ee547b
mohammadbaghershahmir <alialavibarzoki@gmail.com>
2023-08-05 17:01:03 +0430

    Finish Bug Two

---
+++ EndPointStore/Areas/Admin/ViewComponents/Alarm.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Store.Application.Services.ContactsUs.Queries.GetAlarmContactUs;
+using Store.Application.Services.Langueges.Queries;
+
+namespace EndPointStore.Areas.Admin.ViewComponents
+{
+    [ViewComponent(Name = "Alarm")]
+    public class Alarm:ViewComponent
+    {
+        private readonly IGetAlarmContactUsService _getAlarmContactUsService;
+        public Alarm(IGetAlarmContactUsService getAlarmContactUsService)
+        {
+            _getAlarmContactUsService = getAlarmContactUsService;
+        }
+        public IViewComponentResult Invoke()
+        {
+           
+            var Alarms = _getAlarmContactUsService.Execute().Result;
+            return View(viewName: "Alarm",Alarms);
+        }
+    }
+}
